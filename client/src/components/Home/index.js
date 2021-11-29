82cdeb3d0e0b938ffd14dfef1048ec0651c69f56
Michał Matynia <mmatynia@gmail.com>
2021-11-29 19:14:20 +0100

    11

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -27,10 +27,11 @@ export default function Home() {
     const [isWall, setIsWall] = useState();
     const [isLoading, setIsLoading] = React.useState(true);
     const [isLocalUser, setLocalUser] = React.useState();
-    const [isPrevLocalUser, setPrevLocalUser] = React.useState();
     const [isCurrentDetailPage, setCurrentDetailPage] = React.useState();
 
     const setDefaultHomePage = useCallback(async () => {
+
+
         let inQuery = {}
         Object.assign(inQuery, {
             isdefault: { "$eq": true },
@@ -43,204 +44,102 @@ export default function Home() {
         })
 
     }, [dispatch, redux_localeuser])
-    /* Grab Default  */
-
-    React.useEffect(() => {
-        if (
-            !isCurrentDetailPage
-            && !redux_currentdetailpage
-            && !isLocalUser
-            && currentlistpage
-            && redux_localeuser
-            && isLoading
-            && !isWall
-            && document.location.pathname === '/'
-
-        ) {
-
-            console.log('Grab Default Page - current detail');
-            setDefaultHomePage().then((item) => {
-
-                setLocalUser(redux_localeuser)
-                setCurrentDetailPage(item.payload)
 
 
-            })
-        }
-
-    }, [redux_currentdetailpage, currentlistpage, isCurrentDetailPage, isLoading, isLocalUser, isWall, redux_localeuser, setDefaultHomePage])
-  
+    const findPage = useCallback(async () => {
 
 
-
-
-
-
-
-
-
-
-
-
-    // /* Re Render on Link Change */
-    const setNewPage = useCallback(async () => {
         let inQuery = {}
-
         if (redux_currentdetailpage.lgbinder !== '') {
-            Object.assign(inQuery, { lgbinder: { "$eq": redux_currentdetailpage.lgbinder } })
-            //     inQuery = {
-            //         country: { "$eq": value.referenceID.alpha2Code },
-            //         language: { "$eq": value.referenceID.languages[0].iso639_1 }
-            //     }
-            //     if (redux_current_detail_page !== '' && redux_current_detail_page.lgbinder !== '') {
-            //         Object.assign(inQuery, { lgbinder: { "$eq": redux_current_detail_page.lgbinder } })
-            //     } else {
-            //         Object.assign(inQuery, { isdefault: { "$eq": true } })
-
-            //     }
+            Object.assign(inQuery, { 
+                lgbinder: { "$eq": redux_currentdetailpage.lgbinder},
+                country: { "$eq": redux_localeuser.referenceID.alpha2Code },
+                language: { "$eq": redux_localeuser.referenceID.languages[0].iso639_1 }
+             })
+            
 
         } else {
             Object.assign(inQuery, { _id: { "$eq": redux_currentdetailpage._id } })
 
         }
-        Object.assign(inQuery, {
-            lgbinder: { "$eq": redux_currentdetailpage.lgbinder },
-            country: { "$eq": redux_localeuser.referenceID.alpha2Code },
-            language: { "$eq": redux_localeuser.referenceID.languages[0].iso639_1 }
-        });
 
         return await plg_findOne_QueMod({
             model: 'page', dispatch, actionType: 'current_detail', inQuery
         })
 
-    }, [dispatch, redux_currentdetailpage, redux_localeuser ])
+    }, [dispatch, redux_currentdetailpage, redux_localeuser])
+    /* Grab Default  */
 
     React.useEffect(() => {
-
-
         if (
-            // && redux_currentdetailpage
-            // && isLocalUser
-            // && redux_localeuser
-            // && isWall
-
-            !isCurrentDetailPage
-            && redux_currentdetailpage
-            && !isLocalUser
-            && currentlistpage
-            && redux_localeuser
-            && isLoading
-            && !isWall
+            redux_localeuser
             && document.location.pathname === '/'
-
-
         ) {
 
+            // console.log('INSIDE');
+            // console.log(isCurrentDetailPage);
+            // console.log(isLoading);
+
+            if (
+                !isCurrentDetailPage && !redux_currentdetailpage && !isWall && !isLocalUser && isLoading) {
+
+                console.log('Grab Default Page - current detail');
+
+                setLocalUser(redux_localeuser)
+
+                setDefaultHomePage().then((item) => {
+
+                    setCurrentDetailPage(item.payload)
 
 
-            console.log('set New Wall Page');
-            setNewPage().then(()=>{
+                })
+            } else if((isLocalUser !== redux_localeuser) && redux_currentdetailpage && isLocalUser && isWall) {
+                setIsLoading(true)
+
+                setLocalUser(redux_localeuser)
+                findPage().then((item) => {
+                    setIsWall()
+
+                    setCurrentDetailPage(item.payload)
+                    setLocalUser(redux_localeuser)
+
+                })
+            } else if (
+                (
+                    isCurrentDetailPage !== redux_currentdetailpage
+                    && isCurrentDetailPage
+                    && redux_currentdetailpage
+                )
+                ||
+                (
+                    !isCurrentDetailPage
+                    && redux_currentdetailpage
+                    && !isLocalUser
+                )
+                
+                // && isWall
+                // && (!isLocalUser )
+                // && !isLoading
+            ) {
+                console.log('Grab Current Page - current detail');
                 setIsLoading(true)
                 setIsWall()
                 setCurrentDetailPage(redux_currentdetailpage)
-            })
+                setLocalUser(redux_localeuser)
+
+
+
+            }
 
         }
-    },[currentlistpage, isCurrentDetailPage, isLoading, isLocalUser, isWall, redux_currentdetailpage, redux_localeuser, setNewPage])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // /* Grab Default  */
-
-    // React.useEffect(() => {
-    //     if (
-    //         !isCurrentDetailPage
-    //         && !isLocalUser
-    //         && !currentdetailpage
-    //         && currentlistpage
-    //         && redux_localeuser
-    //         && isLoading
-    //         && !isWall
-
-
-    //     ) {
-
-    //         console.log('Set Current Detail Page');
-    //         setDefaultHomePage().then((item) => {
-
-    //             setLocalUser(redux_localeuser)
-    //             // console.log('brejk');
-    //             setCurrentDetailPage(item.payload)
-
-
-    //         })
-    //     }
-
-    // }, [currentdetailpage, currentlistpage, isCurrentDetailPage, isLoading, isLocalUser, isWall, redux_localeuser, setDefaultHomePage])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* Lg Change */
-    // React.useEffect(() => {
-    //     // console.log(isLocalUser);
-    //     // console.log(redux_localeuser);
-
-    //     if (
-    //         isLocalUser !== redux_localeuser
-    //         && isLocalUser
-    //         && currentdetailpage
-    //         && !isLoading
-    //     ) {
-
-    //         console.log('LG CHNAGE Load wall');
-
-    //         setIsLoading(true)
-    //         plg_clearProps({ dispatch, model: 'page', actionType: 'current_detail' })
-    //         // setPrevLocalUser(isLocalUser)
-    //         setLocalUser()
-    //         setIsWall()
-    //         setCurrentDetailPage()
-
-
-    //     }
-
-    // }, [currentdetailpage, dispatch, isLoading, isLocalUser, redux_localeuser])
-
-
-
-
-
-
-
+    }, [currentlistpage, isCurrentDetailPage, isLoading, isLocalUser, isWall, redux_currentdetailpage, redux_localeuser, findPage, setDefaultHomePage])
 
 
 
     const loadPage = useCallback(async () => {
+
+        console.log('runload');
 
         return await loadList_v3({
             dispatch,
@@ -256,12 +155,10 @@ export default function Home() {
 
         if (
             isCurrentDetailPage
-            && currentlistpage
-            && redux_localeuser
             && redux_localeuser.referenceID.currencies[0].code === Object.keys(currencyuser.rates)[0]
             && !isWall
             && isLoading
-            && isLocalUser
+            && isLocalUser === redux_localeuser
         ) {
 
             loadPage().then((allbricks) => {
@@ -276,7 +173,7 @@ export default function Home() {
 
         }
 
-    }, [currencyuser, currentlistpage, isCurrentDetailPage, isLoading, isLocalUser, isWall, loadPage, redux_localeuser]);
+    }, [currencyuser, isCurrentDetailPage, isLoading, isLocalUser, isWall, loadPage, redux_localeuser]);
 
 
 
