b7bc8b16860112ff7d643b143c554f7183c8764e
Michał Matynia <mmatynia@gmail.com>
2021-12-25 12:05:26 +0100

    next 3

--- client/src/components/utils/Email/templates.js
+++ client/src/components/utils/Email/templates.js
@@ -19,139 +19,6 @@ const loopProducts = ({ redux_cartuser, currencySymbol }) => {
 
 // MILKBARDESIGNERS
 
-async function contact_messagebody ({ from, body, subject }) {
-    return ` <!DOCTYPE html>
-    <html style="margin: 0; padding: 0;">
-    <head>
-    <title>One | Contact </title>
-    </head>
-    <body style="margin: 0; padding: 0;">
-            <table class="table" cellpadding="0" cellspacing="0" style="background-color: #eee; empty-cells: hide; margin: 0 auto; padding: 0; width: 1000px;">
-            <tr>
-            <td style="background-color: #999592; margin: 0 auto;">
-            <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Email Received.</h1></td>
-            </tr>
-                <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
-                <p>
-                You have received an email from ${from}
-                </p>
-                <p>
-                ${subject}
-                </p>
-                <p>
-                ${body}
-                </p>
-                <p>
-                www.milkbardesigners.com Mailing system
-                </p>
-                </td></tr>
-            </table>
-            </body></html>`
-}
-
-async function contact_confirmation_email({ redux_cartuser, totalSum, currencySymbol }) {
-    return ` <!DOCTYPE html>
-    <html style="margin: 0; padding: 0;">
-    <head>
-    <title>One | Contact </title>
-    </head>
-    <body style="margin: 0; padding: 0;">
-            <table class="table" cellpadding="0" cellspacing="0" style="background-color: #eee; empty-cells: hide; margin: 0 auto; padding: 0; width: 1000px;">
-            <tr>
-            <td style="background-color: #999592; margin: 0 auto;">
-            <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Email Sent</h1></td>
-            </tr>
-                <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
-                <p>
-                Please allow some time to process your email.
-                </p>
-                <p>
-                Milkbardesigners Team,
-                </p>
-                <p>
-                www.milkbardesigners.com
-                </p>
-                </td></tr>
-            </table>
-            </body></html>`
-}
-
-async function confirmation_email({ redux_cartuser, totalSum, currencySymbol }) {
-    return ` <!DOCTYPE html>
-    <html style="margin: 0; padding: 0;">
-    <head>
-    <title>One | Purchase Completed</title>
-    </head>
-    <body style="margin: 0; padding: 0;">
-            <table class="table" cellpadding="0" cellspacing="0" style="background-color: #eee; empty-cells: hide; margin: 0 auto; padding: 0; width: 1000px;">
-            <tr>
-            <td style="background-color: #999592; margin: 0 auto;">
-            <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Order Summary</h1></td>
-            </tr>
-            <p >
-            You have placed order on the following products in our store:
-            </p>
-            ${loopProducts({ redux_cartuser, currencySymbol })}
-            <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
-            <p>
-                <b>Total Sum: ${totalSum} ${currencySymbol}</b>
-                </p>                </td></tr>
-                <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
-                <p>
-                Thank you for shopping
-                </p>
-                <p>
-                Milkbardesigners
-                </p>
-                <p>
-                www.milkbardesigners.com
-                </p>
-                </td></tr>
-            </table>
-            </body></html>`
-}
-
-
-async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
-
-    return ` <!DOCTYPE html>
-    <html style="margin: 0; padding: 0;">
-    <head>
-    <title>One | Purchase Completed</title>
-    </head>
-    <body style="margin: 0; padding: 0;">
-            <table class="table" cellpadding="0" cellspacing="0" style="background-color: #eee; empty-cells: hide; margin: 0 auto; padding: 0; width: 1000px;">
-            <tr>
-            <td style="background-color: #999592; margin: 0 auto;">
-            <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Order Summary</h1></td>
-            </tr>
-            ${loopProducts({ redux_cartuser, currencySymbol })}
-            <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
-            <p>
-                <b>Total Sum: ${totalSum} ${currencySymbol}</b>
-                </p>                </td></tr>
-                <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
-                <p >
-                Please transfer the order amount to:
-                </p>
-                <p>89 1020 4812 0000 0102 0107 5605
-                </p>
-                <p>
-                Thank you !
-                </p>
-                <p>
-                Milkbardesigners
-                </p>
-                <p>
-                www.milkbardesigners.com
-                </p>
-                </td></tr>
-            </table>
-            </body></html>`
-}
-
-// QUBRICK
-
 // async function contact_messagebody ({ from, body, subject }) {
 //     return ` <!DOCTYPE html>
 //     <html style="margin: 0; padding: 0;">
@@ -166,7 +33,7 @@ async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
 //             </tr>
 //                 <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
 //                 <p>
-//                 Yoy have received an email from ${from}
+//                 You have received an email from ${from}
 //                 </p>
 //                 <p>
 //                 ${subject}
@@ -175,12 +42,13 @@ async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
 //                 ${body}
 //                 </p>
 //                 <p>
-//                 www.qubrick.io Mailing system
+//                 www.milkbardesigners.com Mailing system
 //                 </p>
 //                 </td></tr>
 //             </table>
 //             </body></html>`
-// } 
+// }
+
 // async function contact_confirmation_email({ redux_cartuser, totalSum, currencySymbol }) {
 //     return ` <!DOCTYPE html>
 //     <html style="margin: 0; padding: 0;">
@@ -198,17 +66,17 @@ async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
 //                 Please allow some time to process your email.
 //                 </p>
 //                 <p>
-//                 Qubrick Team,
+//                 Milkbardesigners Team,
 //                 </p>
 //                 <p>
-//                 www.qubrick.io
+//                 www.milkbardesigners.com
 //                 </p>
 //                 </td></tr>
 //             </table>
 //             </body></html>`
 // }
 
-//  async function confirmation_email({ redux_cartuser, totalSum, currencySymbol }) {
+// async function confirmation_email({ redux_cartuser, totalSum, currencySymbol }) {
 //     return ` <!DOCTYPE html>
 //     <html style="margin: 0; padding: 0;">
 //     <head>
@@ -221,7 +89,7 @@ async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
 //             <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Order Summary</h1></td>
 //             </tr>
 //             <p >
-//             You have purchased the following products in our store:
+//             You have placed order on the following products in our store:
 //             </p>
 //             ${loopProducts({ redux_cartuser, currencySymbol })}
 //             <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
@@ -233,14 +101,18 @@ async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
 //                 Thank you for shopping
 //                 </p>
 //                 <p>
-//                 Qubrick Team
+//                 Milkbardesigners
+//                 </p>
+//                 <p>
+//                 www.milkbardesigners.com
 //                 </p>
 //                 </td></tr>
 //             </table>
 //             </body></html>`
-// } 
+// }
 
-//  async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
+
+// async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
 
 //     return ` <!DOCTYPE html>
 //     <html style="margin: 0; padding: 0;">
@@ -268,15 +140,143 @@ async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
 //                 Thank you !
 //                 </p>
 //                 <p>
-//                 Qubrick Team
+//                 Milkbardesigners
 //                 </p>
 //                 <p>
-//                 www.qubrick.io
+//                 www.milkbardesigners.com
 //                 </p>
 //                 </td></tr>
 //             </table>
 //             </body></html>`
-// } 
+// }
+
+// QUBRICK
+
+async function contact_messagebody ({ from, body, subject }) {
+    return ` <!DOCTYPE html>
+    <html style="margin: 0; padding: 0;">
+    <head>
+    <title>One | Contact </title>
+    </head>
+    <body style="margin: 0; padding: 0;">
+            <table class="table" cellpadding="0" cellspacing="0" style="background-color: #eee; empty-cells: hide; margin: 0 auto; padding: 0; width: 1000px;">
+            <tr>
+            <td style="background-color: #999592; margin: 0 auto;">
+            <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Email Received.</h1></td>
+            </tr>
+                <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
+                <p>
+                Yoy have received an email from ${from}
+                </p>
+                <p>
+                ${subject}
+                </p>
+                <p>
+                ${body}
+                </p>
+                <p>
+                www.qubrick.io Mailing system
+                </p>
+                </td></tr>
+            </table>
+            </body></html>`
+} 
+async function contact_confirmation_email({ redux_cartuser, totalSum, currencySymbol }) {
+    return ` <!DOCTYPE html>
+    <html style="margin: 0; padding: 0;">
+    <head>
+    <title>One | Contact </title>
+    </head>
+    <body style="margin: 0; padding: 0;">
+            <table class="table" cellpadding="0" cellspacing="0" style="background-color: #eee; empty-cells: hide; margin: 0 auto; padding: 0; width: 1000px;">
+            <tr>
+            <td style="background-color: #999592; margin: 0 auto;">
+            <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Email Sent</h1></td>
+            </tr>
+                <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
+                <p>
+                Please allow some time to process your email.
+                </p>
+                <p>
+                Qubrick Team,
+                </p>
+                <p>
+                www.qubrick.io
+                </p>
+                </td></tr>
+            </table>
+            </body></html>`
+}
+
+ async function confirmation_email({ redux_cartuser, totalSum, currencySymbol }) {
+    return ` <!DOCTYPE html>
+    <html style="margin: 0; padding: 0;">
+    <head>
+    <title>One | Purchase Completed</title>
+    </head>
+    <body style="margin: 0; padding: 0;">
+            <table class="table" cellpadding="0" cellspacing="0" style="background-color: #eee; empty-cells: hide; margin: 0 auto; padding: 0; width: 1000px;">
+            <tr>
+            <td style="background-color: #999592; margin: 0 auto;">
+            <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Order Summary</h1></td>
+            </tr>
+            <p >
+            You have purchased the following products in our store:
+            </p>
+            ${loopProducts({ redux_cartuser, currencySymbol })}
+            <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
+            <p>
+                <b>Total Sum: ${totalSum} ${currencySymbol}</b>
+                </p>                </td></tr>
+                <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
+                <p>
+                Thank you for shopping
+                </p>
+                <p>
+                Qubrick Team
+                </p>
+                </td></tr>
+            </table>
+            </body></html>`
+} 
+
+ async function purchase_email({ redux_cartuser, totalSum, currencySymbol }) {
+
+    return ` <!DOCTYPE html>
+    <html style="margin: 0; padding: 0;">
+    <head>
+    <title>One | Purchase Completed</title>
+    </head>
+    <body style="margin: 0; padding: 0;">
+            <table class="table" cellpadding="0" cellspacing="0" style="background-color: #eee; empty-cells: hide; margin: 0 auto; padding: 0; width: 1000px;">
+            <tr>
+            <td style="background-color: #999592; margin: 0 auto;">
+            <h1 style="box-sizing: border-box; color: white; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: center; text-transform: uppercase;">Order Summary</h1></td>
+            </tr>
+            ${loopProducts({ redux_cartuser, currencySymbol })}
+            <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
+            <p>
+                <b>Total Sum: ${totalSum} ${currencySymbol}</b>
+                </p>                </td></tr>
+                <tr><td style="box-sizing: border-box; color: black; font-family: Helvetica, Arial, sans-serif; letter-spacing: 0.5px; line-height: 1.4; margin: 0; padding: 15px 25px; text-align: left; text-transform: uppercase;font-size:12px">
+                <p >
+                Please transfer the order amount to:
+                </p>
+                <p>89 1020 4812 0000 0102 0107 5605
+                </p>
+                <p>
+                Thank you !
+                </p>
+                <p>
+                Qubrick Team
+                </p>
+                <p>
+                www.qubrick.io
+                </p>
+                </td></tr>
+            </table>
+            </body></html>`
+} 
 
 
 // SURTARANG
--- server/utils/mail/index.js
+++ server/utils/mail/index.js
@@ -8,9 +8,9 @@ async function sendEmail({ req }) {
     let smtpTransport = nodemailer.createTransport({
         secure: true, // use TLS
         port: 465,
-        host: 'milkbardesigners.com', // mail.tryzna.pl // milkbardesigners.com //qubrick.io // de3.fcomet.com // mail.surtarang.space
+        host: 'qubrick.io', // mail.tryzna.pl // milkbardesigners.com //qubrick.io // de3.fcomet.com // mail.surtarang.space
         auth: {
-            user: 'info@milkbardesigners.com', // info@surtarang.space // info@milkbardesigners.com // info@qubrick.io
+            user: 'info@qubrick.io', // info@surtarang.space // info@milkbardesigners.com // info@qubrick.io
             pass: process.env.EMAIL_PASS // add as Server ENV
 
         },
